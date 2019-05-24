3ca01021cc2a415db67a9579629485f769cc7d62
Robert Jackson <rjackson@linkedin.com>
2019-05-24 19:08:32 -0400

    Basic implementation!

---
+++ bin/rwjblue-release-it-setup.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const execa = require('execa');
+const sortPackageJson = require('sort-package-json');
+const skipInstall = process.argv.includes('--no-install');
+
+const DETECT_TRAILING_WHITESPACE = /\s+$/;
+
+function updatePackageJSON() {
+  let contents = fs.readFileSync('package.json', { encoding: 'utf8' });
+  let trailingWhitespace = DETECT_TRAILING_WHITESPACE.exec(contents);
+  let pkg = JSON.parse(contents);
+
+  pkg.devDependencies = pkg.devDependencies || {};
+  pkg.devDependencies['release-it'] = '^12.2.1';
+  pkg.devDependencies['release-it-lerna-changelog'] = '^1.0.3';
+
+  pkg.scripts = pkg.scripts || {};
+  pkg.scripts.release = 'release-it';
+
+  pkg['release-it'] = {
+    plugins: {
+      'release-it-lerna-changelog': {
+        infile: 'CHANGELOG.md',
+      },
+    },
+    git: {
+      tagName: 'v${version}',
+    },
+    github: {
+      release: true,
+    },
+  };
+  pkg.publishConfig = pkg.publishConfig || {};
+  pkg.publishConfig.registry = pkg.publishConfig.registry || 'https://registry.npmjs.org';
+
+  let sortedPkg = sortPackageJson(pkg);
+  let updatedContents = JSON.stringify(sortedPkg, null, 2);
+
+  if (trailingWhitespace) {
+    updatedContents += trailingWhitespace[0];
+  }
+
+  fs.writeFileSync('package.json', updatedContents, { encoding: 'utf8' });
+}
+
+async function installDependencies() {
+  if (fs.existsSync('yarn.lock')) {
+    await execa('yarn');
+  } else {
+    await execa('npm', ['install']);
+  }
+}
+
+async function main() {
+  if (!fs.existsSync('package.json')) {
+    /* eslint-disable-next-line no-console */
+    console.error(
+      "create-rwjblue-release-it-setup should be ran from within an existing npm package's root directory"
+    );
+    process.exitCode = 1;
+    return;
+  }
+
+  if (!fs.existsSync('CHANGELOG.md')) {
+    fs.writeFileSync('CHANGELOG.md', '', { encoding: 'utf8' });
+  }
+
+  updatePackageJSON();
+
+  if (!skipInstall) {
+    await installDependencies();
+  }
+}
+
+main();
--- tests/bin-test.js
+++ tests/bin-test.js
@@ -1,19 +1,97 @@
+const fs = require('fs');
+const path = require('path');
 const Project = require('fixturify-project');
+const execa = require('execa');
+
+const BIN_PATH = require.resolve('../bin/rwjblue-release-it-setup');
 const ROOT = process.cwd();
 
 QUnit.module('main binary', function(hooks) {
   let project;
 
+  function mergePackageJSON(original, updates) {
+    return Object.assign({}, original, updates, {
+      scripts: Object.assign({}, original.scripts, updates.scripts),
+      publishConfig: Object.assign({}, original.publishConfig, updates.publishConfig),
+      dependencies: Object.assign({}, original.dependencies, updates.dependencies),
+      devDependencies: Object.assign({}, original.devDependencies, updates.devDependencies),
+    });
+  }
+
   hooks.beforeEach(function() {
     project = new Project('some-thing-cool', '0.1.0');
+    project.writeSync();
+    process.chdir(path.join(project.root, project.name));
   });
 
   hooks.afterEach(function() {
     process.chdir(ROOT);
   });
 
-  QUnit.todo('adds devDependencies to package.json', function() {});
-  QUnit.todo('adds CHANGELOG.md file', function() {});
-  QUnit.todo('removes any default header from pre-existing CHANGELOG.md', function() {});
+  QUnit.test('adds CHANGELOG.md file', async function(assert) {
+    assert.notOk(fs.existsSync('CHANGELOG.md'), 'precond - CHANGELOG.md is not present');
+
+    await execa(BIN_PATH, ['--no-install']);
+
+    assert.ok(fs.existsSync('CHANGELOG.md'), 'CHANGELOG.md is present');
+  });
+
+  QUnit.skip('removes prefix from existing CHANGELOG.md', async function(assert) {
+    project.files['CHANGELOG.md'] = `# master\n\n# v1.2.0\n* Foo bar`;
+
+    await execa(BIN_PATH, ['--no-install']);
+
+    assert.strictEqual(
+      fs.readFileSync('CHANGELOG.md', { encoding: 'utf8' }),
+      '# v1.2.0\n* Foo bar',
+      'removes empty prefix from CHANGELOG.md'
+    );
+  });
+
+  QUnit.test('updates the package.json', async function(assert) {
+    let premodificationPackageJSON = JSON.parse(project.toJSON('package.json'));
+
+    await execa(BIN_PATH, ['--no-install']);
+
+    let pkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' }));
+    let expected = mergePackageJSON(premodificationPackageJSON, {
+      devDependencies: {
+        'release-it': '^12.2.1',
+        'release-it-lerna-changelog': '^1.0.3',
+      },
+      scripts: {
+        release: 'release-it',
+      },
+      publishConfig: {
+        registry: 'https://registry.npmjs.org',
+      },
+      'release-it': {
+        plugins: {
+          'release-it-lerna-changelog': {
+            infile: 'CHANGELOG.md',
+          },
+        },
+        git: {
+          tagName: 'v${version}',
+        },
+        github: {
+          release: true,
+        },
+      },
+    });
+
+    assert.deepEqual(pkg, expected);
+  });
+
+  QUnit.test('installs dependencies', async function(assert) {
+    await execa(BIN_PATH);
+
+    assert.ok(fs.existsSync('node_modules/release-it'), 'release-it installed');
+    assert.ok(
+      fs.existsSync('node_modules/release-it-lerna-changelog'),
+      'release-it-lerna-changelog installed'
+    );
+  });
+
   QUnit.todo('adds release-it config to package.json', function() {});
 });
